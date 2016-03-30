f88f109a496bcadf249e61eec75d27734fd3d9e6
makki_d <d.makiuchi@gmail.com>
2016-03-30 20:01:50 +0900

    add LICENSE file

--- aviutlyc.cpp
+++ aviutlyc.cpp
@@ -12,6 +12,8 @@
  *  - ConvertAviUtlYCtoRGBAFix(clip)
  *
  * License (GPL):
+ *  Copyright (C) 2009 Daisuke Makiuchi (MakKi)
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
@@ -25,7 +27,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
- *  http://www.gnu.org/copyleft/gpl.html .
+ *  http://www.gnu.org/licenses/gpl-2.0.html .
  *
  * @file
  *  Main source.
